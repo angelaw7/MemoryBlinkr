f17b0212ea0ca71c6eedc000a74d8f531063bcd7
JakeRead-GH <jakeread99@gmail.com>
2023-09-16 04:48:55 -0400

    Updated flask grid send/receive parameters

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask import jsonify
 import gridgame
 import json
 
 app = Flask(__name__)
+
+global old_grid
 
 @app.route("/")
 def hello_world():
@@ -10,9 +13,12 @@ def hello_world():
 
 @app.route('/start-gridgame', methods=['POST'])
 def sendGridGame():
-    return jsonify(gridgame.generateGrid(arr))
+    global old_grid
+    old_grid = gridgame.generateGrid(5, 5)
+    return jsonify(old_grid)
 
 @app.route('/receive-player-grid', methods=['POST'])
 def receiveGridGame():
-    return jsonify(gridgame.compareGrid(arr))
+    new_grid = request.get_json()
+    return jsonify(gridgame.compareGrid(old_grid, new_grid))
 
