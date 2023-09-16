5d11089825bc4010b83e886196a44ee0317d03b5
hiromikoreyes <114784445+hiromikoreyes@users.noreply.github.com>
2023-09-16 12:06:36 -0400

    added stuff

---
+++ imageGame.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+
+def getErrorScore(img1, img2):
+
+    img1 = cv2.imread(img1)
+    img2 = cv2.imread(img2)
+    h, w = img1.shape
+    diff = cv2.subtract(img2, img1)
+    err = np.sum(diff**2)
+    mse = err/(float(h*w))
+    return mse
---
+++ paragraphGame.py
@@ -0,0 +1,19 @@
+import cohere
+
+
+def generateParagraph(length):
+    api_file = open("apiKey.txt")
+    API_KEY = api_file.readline().strip()
+
+    co = cohere.Client(API_KEY)
+
+    prediction = co.generate(
+                prompt='Give me a random paragraph that ends within the token limit',
+                max_tokens=length)
+
+    paragraph = 'prediction: {}'.format(prediction.generations[0].text)
+    return paragraph
+
+
+def compareParagraph(p1, p2):
+    return False
