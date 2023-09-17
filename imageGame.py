73741b1511b796464bac0469a05d43e92872eb98
Angela Wang <wangela472@gmail.com>
2023-09-17 06:32:59 -0400

    fix draw game bug

--- imageGame.py
+++ imageGame.py
@@ -6,8 +6,7 @@ import numpy as np
 
 
 def getErrorScore(data_1, data_2):
-
-    binary_data_1= base64.b64decode(data_1)
+    binary_data_1 = base64.b64decode(data_1)
     image_buffer_1 = io.BytesIO(binary_data_1)
     p_img1 = Image.open(image_buffer_1)
     arr_1 = np.array(p_img1)
@@ -19,5 +18,5 @@ def getErrorScore(data_1, data_2):
 
     diff = cv2.subtract(arr_2, arr_1)
     err = np.sum(diff**2)
-    mse = err/(float(512*512))
+    mse = err / (float(512 * 512))
     return mse
